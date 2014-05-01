1b4d06eb37cd7fa965aad5c95e9f42f31372124d
Sonia Espinosa <sespinosa@college.harvard.edu>
2014-05-01 15:33:41 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quince::Application.config.secret_key_base = '6e6b07b010e226069331965aec7c54d669a74a296717530b17e77a8c381891bb6f47b4d074b044a961981ce915e7ccd9587deea607f1872d50739e42d259f210'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Quince::Application.config.session_store :cookie_store, key: '_quince_session'
