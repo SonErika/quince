cda0a1af19007197f581ae1fe9a0baff6b04292d
Sonia Espinosa <sespinosa@college.harvard.edu>
2014-05-06 23:38:46 -0400

    fixed pull request issues

--- app/models/dress.rb
+++ app/models/dress.rb
@@ -3,5 +3,5 @@ class Dress < ActiveRecord::Base
   validates :name, presence: true
   validates :size, presence: true
   validates :price, presence: true
-  SIZES = ['US-1', 'US-2', 'US-4', 'US-6', 'US-8', 'US-10', 'US-12']
+  SIZES = w%(US-1, US-2, US-4, US-6, US-8, US-10, US-12)
 end
