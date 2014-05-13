a970b4aac4f4f078dcff4541fa4bd895f938a3b7
Sonia Espinosa <sespinosa@college.harvard.edu>
2014-05-13 15:43:54 -0400

    fix pull request issues

--- app/models/dress.rb
+++ app/models/dress.rb
@@ -8,7 +8,7 @@ class Dress < ActiveRecord::Base
   validates :price_cents, presence: true
   monetize :price_cents
   SIZES = %w(US-1 US-2 US-4 US-6 US-8 US-10 US-12)
-  
+
   def self.unavailable
     where(available: false)
   end
